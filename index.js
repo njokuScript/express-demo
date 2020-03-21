60be34bccaaecad2dcb42c84cceea558d801b7c8
njokuScript <kalunjoku123@gmail.com>
2020-03-21 08:05:18 +0100

    created express server

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+
 const courses = [
   {
     id: 1,
@@ -8,6 +11,11 @@ const courses = [
     title: "chemistry"
   }
 ];
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-const express = require("express");
-const app = express();
+app.listen(3000, () => console.log("Listening on port 3000.."));
