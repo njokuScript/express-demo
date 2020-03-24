387808b9cea7e827c8914c7fd1ede004a500da14
njokuScript <kalunjoku123@gmail.com>
2020-03-24 13:33:47 +0100

    created custom middle ware function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use();
+app.use((req, res, next) => {
+  console.log("Authenticating");
+  next();
+});
 const courses = [
   {
     courseID: 1,
---
+++ logger.js
@@ -0,0 +1,5 @@
+log = (req, res, next) => {
+  console.log("Logging");
+  next();
+};
+module.exports = log;
