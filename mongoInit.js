6b8a5d0e6ac8c7974efc9bc99445fb47c49dfdb7
njokuScript <kalunjoku123@gmail.com>
2020-03-27 14:14:05 +0100

    sync to database

--- mongoInit.js
+++ mongoInit.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("//localhost/npm-app")
+  .connect("mongodb://localhost/npm-app")
   .then(() => console.log("connected to database"))
   .catch(err => console.error("Error connecting to databse", err));
