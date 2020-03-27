4e2baa5f47e957d1bd909b80f943a5eb9886b5f0
njokuScript <kalunjoku123@gmail.com>
2020-03-27 14:20:46 +0100

    created schema

--- mongoInit.js
+++ mongoInit.js
@@ -3,3 +3,11 @@ mongoose
   .connect("mongodb://localhost/npm-app")
   .then(() => console.log("connected to database"))
   .catch(err => console.error("Error connecting to databse", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: true
+});
