b01113e9b2d8224e0c3834c7cc1a36c7151cc99e
njokuScript <kalunjoku123@gmail.com>
2020-04-05 14:12:32 +0100

    used built in validator

--- mongoInit.js
+++ mongoInit.js
@@ -2,14 +2,25 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/npm-app")
   .then(() => console.log("connected to database"))
-  .catch(err => console.error("Error connecting to databse", err));
+  .catch((err) => console.error("Error connecting to databse", err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true, minlength: 5, maxlength: 255 },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
-  isPublished: Boolean
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "blockchain", "mobile"],
+  },
 });
 
 const Course = mongoose.model("Course", courseSchema);
@@ -18,22 +29,27 @@ const createCourse = async () => {
     name: "Decentralized apps",
     author: "Njoku Emmanuel",
     tags: ["solidity", "Tex"],
-    isPublished: true
+    isPublished: true,
+    price: 30,
+    category: "-",
   });
-
-  const result = await course.save();
-  console.log(result);
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
 };
 const pageNumber = 2;
 const pageSize = 10;
 createCourse();
 const getCourse = async () => {
   const courses = await Course.find({
-    author: "Njoku Emmanuel"
+    author: "Njoku Emmanuel",
   })
     .sort({ name: 1, tags: 1 })
     .skip((pageNumber - 1) * pageSize)
     .limit(pageSize);
   console.log(courses);
 };
-getCourse();
+createCourse();
